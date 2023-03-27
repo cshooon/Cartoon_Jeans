2b2a7e88414307a01fd3d11322abfcdc4f571bb1
cshooon <113033780+cshooon@users.noreply.github.com>
2023-03-27 16:16:33 +0900

    feat: 슬라이드 chatgpt 코드 추가

--- cartoon_jeans.py
+++ cartoon_jeans.py
@@ -4,6 +4,19 @@ import cv2
 # Load image
 img = cv2.imread('OMG.jpg')
 
+#chatGPT code
+# 이미지를 회색조로 변경합니다.
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 가우시안 블러(흐리게) 처리합니다.
+gray = cv2.medianBlur(gray, 5)
+# 엣지 검출을 수행합니다.
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+# 컬러 이미지로 변경합니다.
+color = cv2.bilateralFilter(img, 9, 300, 300)
+# 컬러 이미지와 엣지를 합성합니다.
+cartoon = cv2.bitwise_and(color, color, mask=edges)
+# 결과를 출력합니다.
+
 # Color Quantization Filter
 # Reshape the image to a 2D array of pixels and 3 color channels
 pixel_vals = img.reshape((-1, 3))
@@ -39,6 +52,7 @@ stylized = cv2.stylization(median, sigma_s=60, sigma_r=0.07)
 # Show the original and pencil sketch images side by side
 # Display the original and color quantized images
 cv2.imshow('Original', img)
+cv2.imshow("Chatgpt", cartoon)
 cv2.imshow('Color Quantized', res2)
 cv2.imshow('Pencil Sketch', pencil_sketch_img)
 cv2.imshow('Cartoonizing', stylized)
